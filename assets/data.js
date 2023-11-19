0cc0b02529096898ca2b78e87dc8282e09d7f722
AjayPatel4135 <133582441+AjayPatel4135@users.noreply.github.com>
2023-11-19 00:17:28 -0500

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,103 @@
+const festivals = [{
+    id: 1,
+    festival: "Gol Dhana",
+    imageURL: "gujrati-goldhana-img1.webp",
+    imageDescription: "Gol Dhana refers to the sweet treat made with jaggery and coriander seeds that is exchanged between the bride and groom's families at this function which symbolizes an engagement. The bride, along with her family, visits the groom's home with gifts and sweets, wherein they exchange rings and seek blessings from five married women from both families. The ceremony is followed by a sumptuous traditional meal.",
+}, {
+    id: 2,
+    festival: "Lagan Lakhvanu (kankotri)",
+    imageURL: "gujrati-lagan-img1.webp",
+    imageDescription: "This ceremony marks the writing of the first official wedding invite called Kankotri, which is offered to god as a token to invite his blessings to grace the wedding. It also seeks his blessings for the couple and for the wedding to be held successfully on the chosen auspicious date. After the completion of this, official invites to guests are sent out. The ceremony traditionally was held a month before the wedding, but varies now as per convenience.",
+}, {
+    id: 3,
+    festival: "Mehndi",
+    imageURL: "gujrati-mehndi-img1.webp",
+    imageDescription: "Generally hosted two days prior to the wedding at the bride's home, the mehndi is where the women gather for a cheerful function of music and dance as the bride's hands and feet are covered in beautiful henna designs. The groom's initials are often incorporated into the design. The rest of the women also get the hands painted with henna.",
+}, {
+    id: 4,
+    festival: "Sangeet Sandhya",
+    imageURL: "gujrati-sangeet-img1.webp",
+    imageDescription: "A night that everyone looks forward to, the sangeet is a night of music and dance held generally a night before the wedding. Both families get together to let loose, sing songs, play dandiya and garba and have a gala time!",
+}, {
+    id: 5,
+    festival: "Mandap Mahurat",
+    imageURL: "gujrati-mandap-mahurat-img2.webp",
+    imageDescription: "Invoking the blessings of Lord Ganesha before any auspicious occasion is a common practice in Indian/Hindu culture. The bride and groom host a prayer function in their homes respectively and pray to Lord Ganesha, seeking his blessings for a prosperous future with no obstacles. They also pray to Mother Earth to bless the spot where the mandap would be erected.",
+}, {
+    id: 6,
+    festival: "Griha Shanti",
+    imageURL: "gujrati-grih-img2.webp",
+    imageDescription: "Also done in both houses respectively, the 'Griha Shanti Puja' is done to ensure a happy and peaceful marital life for the couple.",
+}, {
+    id: 7,
+    festival: "Pithi",
+    imageURL: "gujrati-pithi-img1.webp",
+    imageDescription: "The 'pithi' or haldi ceremony is held at the bride's and groom's homes separately. They are made to sit on a low stool while their loved ones smear a paste of turmeric, sandalwood, rosewater and other herbs on their face, hands and feet. This paste is prepared traditionally by the bride/ groom's paternal uncle's wife or kaki. They are then bathed in holy water.",
+}, {
+    id: 8,
+    festival: "Mameru",
+    imageURL: "gujrati.webp",
+    imageDescription: "Akin to a tradition present in more Indian cultures, a day before the wedding, the bride's maternal uncle or 'mama' and her maternal aunt's husband called 'mousa' visit her home and gift her a traditional Panetar saree, jewellery, ivory 'chura' (bangles), sweets, dry fruits and so on. The gifts are beautifully wrapped and presented to the bride.",
+}, {
+    id: 9,
+    festival: "Varghodo",
+    imageURL: "gujrati-mameru-img1.webp",
+    imageDescription: "Equivalent to the 'baraat,' the groom travels to the bride's home or the wedding venue on a magnificently decked horse as his relatives dance along with him on the way. Upon reaching, the groom and his family are welcomed by the bride's mother with sweets and aarti in the 'jaan' ritual. In a fun custom, the bride's mother then lightly pinches the groom's nose to remind him to be humble since he is there to ask for her daughter's hand, as he playfully tries to avoid getting caught.",
+}, {
+    id: 10,
+    festival: "Jaimala",
+    imageURL: "gujrati-varmala-img1.webp",
+    imageDescription: "The couple exchange beautiful flower garlands as everyone cheers and showers them with petals.",
+}, {
+    id: 11,
+    festival: "Madhuparka",
+    imageURL: "gujrati-madhuparka-img1.webp",
+    imageDescription: "The groom is then led to the mandap by the bride's mother, where his feet are washed with milk and water and he is fed a holy drink of milk, yogurt, sugar, honey and ghee known as 'panchamrut.'",
+}, {
+    id: 12,
+    festival: "Antarpaat",
+    imageURL: "gujrati-antarpaat-img1.webp",
+    imageDescription: "The bride is led to the mandap by her maternal uncle, after which a cloth is hung up like a curtain between the bride and groom called the 'antarpat.' It is slowly lowered as they gaze upon each other's faces for the first time.",
+}, {
+    id: 13,
+    festival: "Kanyadaan",
+    imageURL: "gujrati-kanyadaan-img1.webp",
+    imageDescription: "The 'kanyadaan' ritual is an important ceremony where the father officially gives his daughter's hand to the groom. After washing their feet, he asks his future son-in-law to take the responsibility of his daughter's happiness and wellbeing from them onwards with folded hands.",
+}, {
+    id: 14,
+    festival: "Hasta Milap",
+    imageURL: "gujrati-hastha-milpa-img1.webp",
+    imageDescription: "After the kanyadaan, the bride's pallu is tied to the groom's shawl by the priest as a holy symbol of their union. He also places their hand in each other's hand and chants mantras as their family showers them with petals and rice grains.",
+}, {
+    id: 15,
+    festival: "Joota Churai",
+    imageURL: "gujrati-joota-chupai-img1.webp",
+    imageDescription: "One of the most fun rituals in Indian weddings, the groom's shoes are 'stolen' by the bride's sisters for money, creating a lot of merriment.",
+}, {
+    id: 16, 
+    festival: "Pheras",
+    imageURL: "gujrati-pheras-img1.webp",
+    imageDescription: "The couple walk around the holy fire as the priest chants from the holy scriptures in the pheras ritual. The brother of the bride hands the couple puffed rice after every circle to offer to the fire.",
+}, {
+    id: 17, 
+    festival: "Saptapadi",
+    imageURL: "gujrati-Saptadi-img1.webp",
+    imageDescription: "A crucial ritual in any Hindu wedding, the couple take seven steps together known as the “saptapadi” as they take seven vows of holy matrimony, binding them for eternity.",
+}, {
+    id: 18, 
+    festival: "Vidaai",
+    imageURL: "gujrati-vidaai-img1.webp",
+    imageDescription: "After the completion of the nuptials, the bride takes leave from her home to go to her new home with her husband. The ceremony sees a tearful goodbye and emotional moments, and she throws puffed rice overhead towards her family, which her mother catches in her pallu.",
+}, {
+    id: 19, 
+    festival: "Ghar ni Laxmi",
+    imageURL: "gujrati-ghar-ni-laxmi-img1.webp",
+    imageDescription: "Translating to 'the goddess of our home,' in this ceremony, the bride is seen as a symbol of Laxmi, the goddess of good fortune. She is welcomed fondly to her new home, which she enters by kicking over a small copper vessel of rice as an auspicious symbol.",
+}, {
+    id: 20, 
+    festival: "Aeki-Baki",
+    imageURL: "gujrati-aeki-baki-img2.webp",
+    imageDescription: "The hectic days unwind with some fun wedding games to truly welcome the bride into the groom's family. A large bowl is filled with milk and a ring is dropped into it. The bride and groom then compete to see who finds it first.",
+}]
+
+export {festivals};
